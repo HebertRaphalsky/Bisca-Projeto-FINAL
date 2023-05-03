185edcb5cc213181ad0daebd2614238156ab7ee0
Hebert Souza Raphalsky Do Nascimento <hebeyeemo@gmail.com>
2023-05-03 12:58:24 -0300

    Game incompleto

---
+++ Game.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.Random;
+import java.util.Stack;
+
+/**
+ * Game
+ */
+public class Game {
+    private static Stack<Carta> _Mao;
+    public static Carta _Trunfo;
+    private Player _p1, _p2, _p3, _p4;
+    private static final String[] _Nipes = { "Ouro", "Paus", "Copas", "Espadas" };
+
+    /**
+     * @param p1
+     * @param p2
+     */
+    public Game(Player p1, Player p2, Player p3, Player p4) {
+        this._DarMao();
+        this._p1 = p1;
+        this._p2 = p2;
+        this._p3 = p3;
+        this._p4 = p4;
+    }
+
+/**
+     * @return
+     */
+    public Player play() {
+        Carta playedFirst, playedSecond, playedThird, playedFourt;
+        int Resultado;
+        this._chooseStarter();
+        this._giveCartas();
+        this._Trunfo = _Mao.firstElement();
+
+        System.out.println("----------------------------");
+        System.out.println("*   Carta Trunfo é " + this._Trunfo + "   *");
+        System.out.println("----------------------------");
